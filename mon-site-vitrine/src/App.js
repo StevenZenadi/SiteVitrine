e7b47b7877c89adbf90e187add4e020a6a0a3e82
StevenZenadi <32456744+StevenZenadi@users.noreply.github.com>
2025-02-21 12:57:08 +0000

    Ajout du header dans le build

--- mon-site-vitrine/src/App.js
+++ mon-site-vitrine/src/App.js
@@ -1,3 +1,5 @@
+/**build mode********************************************* */
+
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -17,6 +19,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter basename="/SiteVitrine">
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projets" element={<Projects />} />
@@ -24,9 +27,51 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-      {/*<Player />*/}
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+{/*
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Import des pages
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Player from "./components/Player";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
+*/}
+
